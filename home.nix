4ff160f5a237161f56c6bc0fa21e1638e2fbf967
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2024-07-11 12:52:11 +0530

    initial

---
+++ home.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, ... }:
+let 
+user = "subwave";
+in
+{
+  home = {
+    packages = with pkgs; [
+      home-manager
+      neovim
+      hello
+    ];
+
+    # This needs to actually be set to your username
+    username = "${user}";
+    homeDirectory = "/home/${user}";
+
+    # You do not need to change this if you're reading this in the future.
+    # Don't ever change this after the first build.  Don't ask questions.
+    stateVersion = "24.05";
+  };
+}
