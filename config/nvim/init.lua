acc2f410fd7d7aa88fd760b91fb76189a5926cd4
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2024-08-03 21:19:21 -0700

    update nvim with jupy

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -234,6 +234,16 @@ require('lazy').setup({
   },
 
   {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- this is an example, not a default. Please see the readme for more configuration options
+      vim.g.molten_output_win_max_height = 12
+    end,
+  },
+
+  {
     'akinsho/toggleterm.nvim',
     version = "*",
     config = true,
