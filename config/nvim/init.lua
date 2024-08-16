698e8eeefb40eb0ae4448d08f8ebeb579947b3f8
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2024-08-16 13:23:07 -0700

    pico8 3

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1190,9 +1190,25 @@ end
 
 TextWidthConfig()
 
-vim.filetype.add { extension = { p8 = "pico8" } } -- add pico8 file type
+-- Setup pico8 editing with pico8-ls and lua for syntax
+vim.treesitter.language.register("lua", "pico8")
+vim.filetype.add { extension = { p8 = "pico8" } }
+vim.api.nvim_create_autocmd({ 'BufNew', 'BufEnter' }, {
+  pattern = { '*.p8' },
+  callback = function(args)
+    vim.lsp.start({
+      name = 'pico8-ls',
+      cmd = { 'pico8-ls', '--stdio' },
+      root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
+      -- Setup your keybinds in the on_attach function
+      on_attach = on_attach,
+    })
+  end
+})
 
+-- set colorscheme
 vim.cmd.colorscheme(colorscheme)
+
 
 local highlight = vim.api.nvim_set_hl
 highlight(0, 'FloatBorder', { link = 'Normal' })
