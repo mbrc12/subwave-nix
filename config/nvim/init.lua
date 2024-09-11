4a86b654faa24d31be7ca2ce893fc60926181250
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2024-09-11 13:43:56 -0700

    jsonls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -692,10 +692,9 @@ require('lazy').setup({
           },
         },
 
-        pico8_ls = {
-        },
+        tsserver = {},
 
-        tsserver = {}
+        jsonls = {},
 
       }
 
@@ -753,6 +752,7 @@ require('lazy').setup({
           }
         }
       })
+
 
       -- require("lspconfig").lspai.setup {}
     end,
@@ -1196,20 +1196,26 @@ end
 
 TextWidthConfig()
 
--- Setup pico8 editing with pico8-ls and lua for syntax
-vim.treesitter.language.register("lua", "pico8")
-vim.filetype.add { extension = { p8 = "pico8" } }
-vim.api.nvim_create_autocmd({ 'BufNew', 'BufEnter' }, {
-  pattern = { '*.p8' },
-  callback = function(args)
-    vim.lsp.start({
-      name = 'pico8-ls',
-      cmd = { 'pico8-ls', '--stdio' },
-      root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
-      -- Setup your keybinds in the on_attach function
-      on_attach = on_attach,
-    })
-  end
+-- -- Setup pico8 editing with pico8-ls and lua for syntax
+-- vim.treesitter.language.register("lua", "pico8")
+-- vim.filetype.add { extension = { p8 = "pico8" } }
+-- vim.api.nvim_create_autocmd({ 'BufNew', 'BufEnter' }, {
+--   pattern = { '*.p8' },
+--   callback = function(args)
+--     vim.lsp.start({
+--       name = 'pico8-ls',
+--       cmd = { 'pico8-ls', '--stdio' },
+--       root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
+--       -- Setup your keybinds in the on_attach function
+--       on_attach = on_attach,
+--     })
+--   end
+-- })
+
+vim.filetype.add({
+  extension = {
+    gotmpl = 'gotmpl',
+  }
 })
 
 -- set colorscheme
