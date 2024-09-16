bcf9430767a410f02be6f30942de29d9d95e6110
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2024-09-15 20:25:22 -0700

    fuzzy find telescope

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -437,10 +437,9 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'search help' })
       -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'search files' })
-      -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'live grep' })
+      vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, { desc = 'fuzzy search in current file' })
       vim.keymap.set('n', '<F9>', builtin.diagnostics, { desc = 'search diagnostics' })
       vim.keymap.set('n', '<F12>', builtin.resume, { desc = 'telescope resume' })
       -- vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
